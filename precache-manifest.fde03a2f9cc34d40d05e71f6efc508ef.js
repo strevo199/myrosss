2ce2b8b219b5c501fdfe13ad9702f2b221cfe0bf
Stephen Mgbeojikwe <mgbeojikwestephen@gmail.com>
2021-05-22 11:20:13 +0100

    Updates

---
+++ precache-manifest.fde03a2f9cc34d40d05e71f6efc508ef.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ee91b73feb0c05068e3f74ed8b9c991",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7eea8d6ff1679c458bea",
+    "url": "/static/css/main.6d72ea8c.chunk.css"
+  },
+  {
+    "revision": "ceb67ac57a9c172664d1",
+    "url": "/static/js/2.a5d193e1.chunk.js"
+  },
+  {
+    "revision": "0b32cc9b9554144821716e237940a07c",
+    "url": "/static/js/2.a5d193e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7eea8d6ff1679c458bea",
+    "url": "/static/js/main.2396dd9b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "a0fd9d33899690f130a2f59f236bf0ce",
+    "url": "/static/media/B_QLogo.a0fd9d33.png"
+  },
+  {
+    "revision": "bcd5c06413b6377eef3e948311dfa8dd",
+    "url": "/static/media/BlueDesign.bcd5c064.png"
+  },
+  {
+    "revision": "6f4089b1f3ad3cf357c1823e3272d2c6",
+    "url": "/static/media/ImageMask.6f4089b1.png"
+  },
+  {
+    "revision": "79e3a94f60c9ab37d1771af376dfa9dd",
+    "url": "/static/media/Logo.79e3a94f.png"
+  },
+  {
+    "revision": "40b1849fdbd620734440d155b7bda433",
+    "url": "/static/media/YellowDesign.40b1849f.png"
+  }
+]);
